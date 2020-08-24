f33174bf8d8072876ae162f7cd9b3330a6641568
KVlearn <kavya.mdu@gmail.com>
2020-08-24 15:47:06 -0700

    Added Responsive design

---
+++ team.js
@@ -0,0 +1,12 @@
+function myToggle() {
+    const navs = document.querySelectorAll('.NavbarItems') /* Gets the 3 items Home,Login and About Us*/
+    navs.forEach(nav => nav.classList.toggle('NavbarToggleShow')); 
+  } /* note . is not needed*/
+  
+  document.querySelector('.NavbarLink-toggle')/*NavbarToggleShow to display on click event*/
+    .addEventListener('click', myToggle); /*on clickevent call classToggle fucntion */
+  
+  /*1. element.classList returns a live DOM collection of the class attributes of the element.
+  2.Toggle between adding and removing a class name from an element with JavaScript.*/
+  /*https://www.w3schools.com/howto/howto_js_toggle_class.asp*/
+  
